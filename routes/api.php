48015dc570a97a31e5030f9078b4c1a78b215b7e
ngodangngoding <dfmaulanaa@gmail.com>
2026-03-30 23:40:34 +0700

    feature: add Notes Controller & api route

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,9 @@
 <?php
 use App\Http\Controllers\BrandController;
+use App\Http\Controllers\NoteController;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('brands', BrandController::class);
-
+Route::apiResource('notes', NoteController::class);
 
 ?>
