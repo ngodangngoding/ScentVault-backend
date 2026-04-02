1a6b893d57c30eed47ea6c638899aa9437825733
ngodangngoding <dfmaulanaa@gmail.com>
2026-04-02 15:41:33 +0700

    feat: add api resource for perfume and notes

---
+++ app/Http/Resources/PerfumeResource.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PerfumeResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'brand_id' => $this->brand_id,
+            'concentration' => $this->concentration,
+            'description' => $this->description,
+            'image' => $this->image,
+            'image_url' => $this->image_url,
+            'is_active' => $this->is_active,
+            'star_rating' => $this->star_rating,
+            'notes' => PerfumeNoteResource::collection($this->whenLoaded('perfumeNote')),
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at
+        ];
+    }
+}
