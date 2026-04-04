6343e43a1dfc7aa7b6652907e7762042773ca6e5
ngodangngoding <dfmaulanaa@gmail.com>
2026-04-04 21:53:18 +0700

    refactor: fix parameter typo

--- app/Http/Resources/UserPerfumeResource.php
+++ app/Http/Resources/UserPerfumeResource.php
@@ -18,7 +18,7 @@ class UserPerfumeResource extends JsonResource
             'id' => $this->id,
             'name' => $this->name,
             'brand_name' => $this->brand->name,
-            'concentraition' => $this->concentration,
+            'concentration' => $this->concentration,
             'description' => $this->description,
             'image_url' => $this->image_url,
             'is_active' => $this->pivot->is_active,
