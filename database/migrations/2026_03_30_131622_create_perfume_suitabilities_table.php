3d4367b4251848098ae3c6ca4b393c67110f89fe
ngodangngoding <dfmaulanaa@gmail.com>
2026-04-04 22:28:47 +0700

    refactor: add ideal_temperature column

--- database/migrations/2026_03_30_131622_create_perfume_suitabilities_table.php
+++ database/migrations/2026_03_30_131622_create_perfume_suitabilities_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use function Laravel\Prompts\table;
 
 return new class extends Migration {
     /**
@@ -13,6 +14,7 @@ return new class extends Migration {
         Schema::create('perfume_suitabilities', function (Blueprint $table) {
             $table->id();
             $table->foreignId('perfume_id');
+            $table->enum('ideal_temperature', ['dingin', 'normal', 'panas']);
             $table->enum('ideal_time', ['pagi', 'siang', 'malam']);
             $table->enum('ideal_environment', ['indoor', 'outdoor', 'all around']);
             $table->timestamps();
