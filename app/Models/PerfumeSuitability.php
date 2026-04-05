89a93264fdc7735c33b0e07443cd3301007f2e8f
ngodangngoding <dfmaulanaa@gmail.com>
2026-04-05 21:16:59 +0700

    refactore: add ideal_temperature to fillable and remove weather_id

--- app/Models/PerfumeSuitability.php
+++ app/Models/PerfumeSuitability.php
@@ -6,16 +6,11 @@ use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-#[Fillable(['perfume_id', 'weather_id', 'ideal_time', 'ideal_environment'])]
+#[Fillable(['perfume_id', 'ideal_temperature', 'ideal_time', 'ideal_environment'])]
 class PerfumeSuitability extends Model
 {
     public function perfume(): BelongsTo
     {
         return $this->belongsTo(Perfume::class);
-    }
-
-    public function weather(): BelongsTo
-    {
-        return $this->belongsTo(Weather::class);
     }
 }
