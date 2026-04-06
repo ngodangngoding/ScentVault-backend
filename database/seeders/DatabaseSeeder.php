8bbd2b30dda58d34c66d582aee38622efcfe86bb
ngodangngoding <dfmaulanaa@gmail.com>
2026-04-06 17:39:08 +0700

    refactor: update routes, middleware config, and cleanup deprecated weather module

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,7 +17,8 @@ class DatabaseSeeder extends Seeder
             BrandSeeder::class,
             NoteSeeder::class,
             OccasionSeeder::class,
-            UserSeeder::class
+            UserSeeder::class,
+            RuleConfigSeeder::class,
         ]);
 
     }
--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,11 @@ use App\Http\Controllers\BrandController;
 use App\Http\Controllers\NoteController;
 use App\Http\Controllers\OccasionController;
 use App\Http\Controllers\PerfumeController;
+use App\Http\Controllers\PerfumeSuitabilityController;
 use App\Http\Controllers\RegionController;
 use App\Http\Controllers\ScentLogController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\RuleConfigController;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('brands', BrandController::class);
@@ -14,10 +17,18 @@ Route::apiResource('occasions', OccasionController::class);
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
+
 Route::middleware('auth:sanctum')->group(function () {
     Route::apiResource('perfumes', PerfumeController::class);
+    Route::put('/perfumes/{perfume}/suitability', [PerfumeSuitabilityController::class, 'update']);
+    Route::get('perfumes/{perfume}/suitability', [PerfumeSuitabilityController::class, 'show']);
     Route::apiResource('scentLog', ScentLogController::class);
     Route::post('/logout', [AuthController::class, 'logout']);
+});
+
+Route::middleware(['auth:sanctum', 'admin'])->group(function () {
+    Route::apiResource('users', UserController::class);
+    Route::apiResource('rule-configs', RuleConfigController::class);
 });
 
 Route::prefix('region')->group(function () {
@@ -26,3 +37,4 @@ Route::prefix('region')->group(function () {
     Route::get('/districts', [RegionController::class, 'districts']);
     Route::get('/villages', [RegionController::class, 'villages']);
 });
+
